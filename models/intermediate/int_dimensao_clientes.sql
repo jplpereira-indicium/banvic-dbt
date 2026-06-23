a20ff4ef3498efd39d7b1e2e3ff6571cb1eaeedd
João Pedro Pereira <joao.lima@indicium.ai>
2026-06-23 14:34:41 +0000

    finalizada a dimensão de clientes

---
+++ models/intermediate/int_dimensao_clientes.sql
@@ -0,0 +1,31 @@
+with
+    clientes as (
+        select *
+        from {{ ref('stg_erp__clientes')}}
+    )
+
+    , localidades as (
+        select *
+        from {{ ref('stg_erp__localidades')}}
+    )
+
+    , clientes_enriquecido as (
+        select 
+            clientes.pk_cod_cliente
+            , clientes.nome_cliente
+            , clientes.email_cliente
+            , clientes.tipo_cliente
+            , clientes.ts_inclusao
+            , clientes.cpfcnpj_cliente
+            , clientes.data_nascimento_cliente
+            , clientes.endereco_cliente
+            , clientes.cep_cliente
+            , localidades.cidade as cidade_cliente
+            , localidades.uf as uf_cliente
+        from clientes
+        left join localidades
+            on localidades.pk_localidade = clientes.fk_localidade
+    )
+
+select *
+from clientes_enriquecido
